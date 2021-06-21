c5149c0ea70a391ac23c5e7eab8bb6a1312f6ba5
v_yuanwenbin <v_yuanwenbin@baidu.com>
2021-06-21 14:04:39 +0800

    add mdbx for test

--- include/GQLlite.h
+++ include/GQLlite.h
@@ -1,6 +1,24 @@
 #pragma once
+#include <stdint.h>
 
-void insert();
-void update();
-void drop();
-void query();
+struct gqlite;
+
+enum gqlite_increase_mode {
+    gm_random,
+    gm_unscale,
+};
+
+typedef uint32_t (*gqlite_distance)(uint32_t pt1, uint32_t pt2);
+
+typedef struct _gqlite_model {
+    gqlite_increase_mode mode;
+    gqlite_distance fdist;
+} gqlite_model;
+
+int gqlite_open(const char* filename, gqlite** ppDb, gqlite_model mode);
+int gqlite_create(gqlite* pDb, const char* gql);
+void gqlite_insert(gqlite* pDb);
+void gqlite_update(gqlite* pDb);
+void gqlite_drop(gqlite* pDb);
+void gqlite_query(gqlite* pDb);
+int gqlite_close(gqlite*);
---
+++ src/GQLlite.cpp
@@ -0,0 +1,9 @@
+#include "GQLlite.h"
+
+int gqlite_open(const char* filename, gqlite** ppDb, gqlite_model mode)
+{
+    if (mode.fdist == nullptr) {
+        
+    }
+    return 0;
+}
